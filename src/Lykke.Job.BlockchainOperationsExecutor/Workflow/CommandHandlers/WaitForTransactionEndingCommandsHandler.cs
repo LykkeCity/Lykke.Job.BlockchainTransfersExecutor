44538bcb8f840bb6ac72e732f0b4c71184c20600
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2018-01-26 17:39:30 +0300

    BlockchainApi.Client is updated

--- src/Lykke.Job.BlockchainOperationsExecutor/Workflow/CommandHandlers/WaitForTransactionEndingCommandsHandler.cs
+++ src/Lykke.Job.BlockchainOperationsExecutor/Workflow/CommandHandlers/WaitForTransactionEndingCommandsHandler.cs
@@ -41,8 +41,7 @@ namespace Lykke.Job.BlockchainOperationsExecutor.Workflow.CommandHandlers
             // TODO: Check for the availability of the tranaction rebuilding function and publish 
             // TransactionTimeoutEvent after configured timeout to run transaction rebuild process path
 
-            var transaction =
-                await apiClient.TryGetBroadcastedTransactionAsync(command.OperationId, blockchainAsset);
+            var transaction = await apiClient.TryGetBroadcastedTransactionAsync(command.OperationId, blockchainAsset);
 
             if (transaction == null)
             {
