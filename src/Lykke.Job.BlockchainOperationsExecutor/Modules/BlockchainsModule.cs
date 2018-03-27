5336ce597dc25251a029ae49d23f88d619f5db72
Yury Glushkov <mail@glushkov.us>
2018-03-27 16:51:50 +0300

    BlockchainSignFacade support added

--- src/Lykke.Job.BlockchainOperationsExecutor/Modules/BlockchainsModule.cs
+++ src/Lykke.Job.BlockchainOperationsExecutor/Modules/BlockchainsModule.cs
@@ -4,21 +4,29 @@ using Common.Log;
 using Lykke.Job.BlockchainOperationsExecutor.Core.Services.Blockchains;
 using Lykke.Job.BlockchainOperationsExecutor.Services.Blockchains;
 using Lykke.Job.BlockchainOperationsExecutor.Settings.Blockchain;
+using Lykke.Job.BlockchainOperationsExecutor.Settings.JobSettings;
+using Lykke.Job.BlockchainOperationsExecutor.Settings.SignFacade;
 using Lykke.Service.BlockchainApi.Client;
-using Lykke.Service.BlockchainSignService.Client;
+using Lykke.Service.BlockchainSignFacade.Client;
 
 namespace Lykke.Job.BlockchainOperationsExecutor.Modules
 {
     public class BlockchainsModule : Module
     {
+        private readonly BlockchainOperationsExecutorSettings _blockchainOperationsExecutorSettings;
         private readonly BlockchainsIntegrationSettings _blockchainsIntegrationSettings;
+        private readonly BlockchainSignFacadeClientSettings _blockchainSignFacadeClientSettings;
         private readonly ILog _log;
 
         public BlockchainsModule(
+            BlockchainOperationsExecutorSettings blockchainOperationsExecutorSettings,
             BlockchainsIntegrationSettings blockchainsIntegrationSettings,
+            BlockchainSignFacadeClientSettings blockchainSignFacadeClientSettings,
             ILog log)
         {
+            _blockchainOperationsExecutorSettings = blockchainOperationsExecutorSettings;
             _blockchainsIntegrationSettings = blockchainsIntegrationSettings;
+            _blockchainSignFacadeClientSettings = blockchainSignFacadeClientSettings;
             _log = log;
         }
 
@@ -27,23 +35,29 @@ namespace Lykke.Job.BlockchainOperationsExecutor.Modules
             builder.RegisterType<BlockchainApiClientProvider>()
                 .As<IBlockchainApiClientProvider>();
 
-            builder.RegisterType<BlockchainSignServiceClientProvider>()
-                .As<IBlockchainSignServiceClientProvider>();
+            builder.RegisterInstance(CreateBlockchainSignFacadeClient())
+                .AsSelf();
+            
 
             foreach (var blockchain in _blockchainsIntegrationSettings.Blockchains.Where(b => !b.IsDisabled))
             {
                 _log.WriteInfo("Blockchains registration", "",
-                    $"Registering blockchain: {blockchain.Type} -> \r\nAPI: {blockchain.ApiUrl}\r\nSign: {blockchain.SignFacadeUrl}\r\nHW: {blockchain.HotWalletAddress}");
+                    $"Registering blockchain: {blockchain.Type} -> \r\nAPI: {blockchain.ApiUrl}\r\nHW: {blockchain.HotWalletAddress}");
 
                 builder.RegisterType<BlockchainApiClient>()
                     .Named<IBlockchainApiClient>(blockchain.Type)
                     .WithParameter(TypedParameter.From(blockchain.ApiUrl));
-
-                builder.RegisterType<BlockchainSignServiceClient>()
-                    .Named<IBlockchainSignServiceClient>(blockchain.Type)
-                    .WithParameter(new NamedParameter("serviceUrl", blockchain.SignFacadeUrl))
-                    .WithParameter(new NamedParameter("apiKey", blockchain.SignFacadeApiKey));
             }
+        }
+
+        private IBlockchainSignFacadeClient CreateBlockchainSignFacadeClient()
+        {
+            return new BlockchainSignFacadeClient
+            (
+                hostUrl: _blockchainSignFacadeClientSettings.ServiceUrl,
+                apiKey: _blockchainOperationsExecutorSettings.SignFacadeApiKey,
+                log: _log
+            );
         }
     }
 }
