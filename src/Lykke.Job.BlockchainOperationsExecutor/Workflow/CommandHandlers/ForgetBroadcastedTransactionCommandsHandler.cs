38fc161ba5b20d396fe194d538abcafa5361eb9c
Andrey Krivoshapov <andrey.krivoshapov@lykke.com>
2018-07-25 11:50:29 +0300

    Implement exclusive withdrawals locking

--- src/Lykke.Job.BlockchainOperationsExecutor/Workflow/CommandHandlers/ForgetBroadcastedTransactionCommandsHandler.cs
+++ src/Lykke.Job.BlockchainOperationsExecutor/Workflow/CommandHandlers/ForgetBroadcastedTransactionCommandsHandler.cs
@@ -3,6 +3,7 @@ using JetBrains.Annotations;
 using Lykke.Common.Chaos;
 using Lykke.Cqrs;
 using Lykke.Job.BlockchainOperationsExecutor.Contract.Events;
+using Lykke.Job.BlockchainOperationsExecutor.Core.Domain;
 using Lykke.Job.BlockchainOperationsExecutor.Core.Services.Blockchains;
 using Lykke.Job.BlockchainOperationsExecutor.Workflow.Commands;
 
@@ -13,19 +14,35 @@ namespace Lykke.Job.BlockchainOperationsExecutor.Workflow.CommandHandlers
     {
         private readonly IChaosKitty _chaosKitty;
         private readonly IBlockchainApiClientProvider _apiClientProvider;
+        private readonly ICapabilitiesService _capabilitiesService;
+        private readonly ISourceAddresLocksRepoistory _locksRepoistory;
 
         public ForgetBroadcastedTransactionCommandsHandler(
             IChaosKitty chaosKitty,
-            IBlockchainApiClientProvider apiClientProvider)
+            IBlockchainApiClientProvider apiClientProvider,
+            ICapabilitiesService capabilitiesService,
+            ISourceAddresLocksRepoistory locksRepoistory)
         {
             _chaosKitty = chaosKitty;
             _apiClientProvider = apiClientProvider;
+            _capabilitiesService = capabilitiesService;
+            _locksRepoistory = locksRepoistory;
         }
 
         [UsedImplicitly]
         public async Task<CommandHandlingResult> Handle(ForgetBroadcastedTransactionCommand command, IEventPublisher publisher)
         {
             var apiClient = _apiClientProvider.Get(command.BlockchainType);
+
+            var capabilities = await _capabilitiesService.GetAsync(command.BlockchainType);
+            if (capabilities.IsExclusiveWithdrawalsRequired)
+            {
+                await _locksRepoistory.ReleaseLockAsync(command.BlockchainType, command.FromAddress, command.OperationId);
+                _chaosKitty.Meow(command.OperationId);
+
+                await _locksRepoistory.ReleaseLockAsync(command.BlockchainType, command.ToAddress, command.OperationId);
+                _chaosKitty.Meow(command.OperationId);
+            }
 
             await apiClient.ForgetBroadcastedTransactionsAsync(command.OperationId);
 
