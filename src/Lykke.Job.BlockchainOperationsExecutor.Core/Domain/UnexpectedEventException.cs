4744b5cbb967f21a4be5d2d0b75ecf66aa3cc165
Dmitry Samodov <samodov.dmitry@gmail.com>
2021-02-09 19:50:58 +0300

    LM-690: added errors interceptors

---
+++ src/Lykke.Job.BlockchainOperationsExecutor.Core/Domain/UnexpectedEventException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lykke.Job.BlockchainOperationsExecutor.Core.Domain
+{
+    public class UnexpectedEventException : Exception
+    {
+        public UnexpectedEventException()
+        {
+        }
+
+        public UnexpectedEventException(string message):base(message)
+        {
+        }
+    }
+}
