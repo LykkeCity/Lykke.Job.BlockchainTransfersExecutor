c8e59f98dfed458975f4941a9b3a6dd53850703c
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2021-02-18 17:32:55 +0300

    Added an option to enforce operation rebuilding

---
+++ src/Lykke.Job.BlockchainOperationsExecutor.Core/Domain/ITransactionsToRebuildRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Lykke.Job.BlockchainOperationsExecutor.Core.Domain
+{
+    public interface ITransactionsToRebuildRepository
+    {
+        Task<IReadOnlyCollection<Guid>> GetAll();
+        Task AddOrReplace(Guid operationId);
+        Task EnsureRemoved(Guid operationId);
+        Task<bool> Contains(Guid operationId);
+    }
+}
